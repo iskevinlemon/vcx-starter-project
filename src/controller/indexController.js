f6eeea5716e615b0d2a2ce3bee23d3bcd31f62cf
KevinGit <kevin@eaucole.com>
2024-04-09 04:08:39 +0700

    Added a counter example

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -3,3 +3,13 @@ Controller.RegisterView({
 });
 
 Controller.Bind("title", "VCX Starter Project");
+
+
+// Counter
+var count = 0;
+Controller.Bind("count", count);
+
+function addCount(){
+  count ++;
+  Controller.Bind("count", count);
+}
