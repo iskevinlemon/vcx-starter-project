8b382605379fbb9f524e62bc6c5aa8571fddaaee
KevinGit <kevin@eaucole.com>
2024-04-21 02:04:58 +0800

    Changed bundle script

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -4,12 +4,16 @@ Controller.RegisterView({
 
 Controller.Bind("title", "VCX Starter Project");
 
-
 // Counter
 var count = 0;
 Controller.Bind("count", count);
 
 function addCount(){
   count ++;
+  Controller.Bind("count", count);
+}
+
+function resetCount(){
+  count = 0;
   Controller.Bind("count", count);
 }
